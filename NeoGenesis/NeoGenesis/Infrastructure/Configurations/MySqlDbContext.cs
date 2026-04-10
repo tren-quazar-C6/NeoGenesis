9b6f9064332c6da601314737777eaf50d7adc537
stteenArts <stteenriwi@gmail.com>
2026-04-09 21:07:23 -0500

    configurated: Conection to Database succesfully

--- NeoGenesis/NeoGenesis/Infrastructure/Configurations/MySqlDbContext.cs
+++ NeoGenesis/NeoGenesis/Infrastructure/Configurations/MySqlDbContext.cs
@@ -1,5 +1,6 @@
 using NeoGenesis.Infrastructure.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 
 namespace NeoGenesis.Infrastructure.Configurations;
@@ -7,10 +8,20 @@ namespace NeoGenesis.Infrastructure.Configurations;
 public class MySqlDbContext : DbContext
 {
     public DbSet<Dinosaur> Dinosaurs { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseMySql(
-        "server=;database=db_robinson_andres_cortes;user=root;password=",
-        ServerVersion.AutoDetect("server=;database=db_robinson_andres_cortes;user=root;password=")
-    );
+    public MySqlDbContext(DbContextOptions<MySqlDbContext> options) : base(options) { }
 }
 
+public class MySqlDbContextFactory : IDesignTimeDbContextFactory<MySqlDbContext>
+{
+    public MySqlDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<MySqlDbContext>();
+
+        optionsBuilder.UseMySql(
+            "server=204.168.211.73;database=neogenesis;user=root;password=gWTeX0zTHgGQ6G1",
+            new MySqlServerVersion(new Version(8, 0, 0)) // Mysql Version
+        );
+
+        return new MySqlDbContext(optionsBuilder.Options);
+    }
+}
--- NeoGenesis/NeoGenesis/Infrastructure/Models/Dinosaur.cs
+++ NeoGenesis/NeoGenesis/Infrastructure/Models/Dinosaur.cs
@@ -3,8 +3,17 @@ namespace NeoGenesis.Infrastructure.Models;
 public class Dinosaur
 {
     public int Id { get; set; }
+    public string UserName { get; set; }
     public string Name { get; set; }
-    public string species { get; set; }
+    public string Email { get; set; }
+    public string Species { get; set; }
     public int Age { get; set; }
+    public string Type { get; set; }
+    public string  Zone { get; set; }
+    public string Address { get; set; } = String.Empty;
+    public string TrackNumber { get; set; } = String.Empty;
+    public string Password  { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdateAt { get; set; }
     
 }
